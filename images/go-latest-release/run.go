f26ebb88a3452d4d75dac53fe6a5e89ee3d5107f
h2so5 <mail@h2so5.net>
2014-11-06 00:52:22 +0900

    Add go-dev

---
+++ images/go-latest-release/run.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"strings"
+
+	"../../agent"
+)
+
+func build(files []string, in agent.Input, out *agent.Output) (string, []string) {
+	var args []string = []string{
+		"build",
+		"-o",
+		"main",
+	}
+
+	if race, ok := in.Options["race"].(bool); ok {
+		if race {
+			args = append(args, "-race")
+		}
+	}
+
+	return "go", append(args, files...)
+}
+
+func run([]string, agent.Input, *agent.Output) (string, []string) {
+	return "./main", nil
+}
+
+func version() string {
+	v, _ := agent.System(".", "", "go", "version")
+	v = strings.Replace(v, "go version", "", -1)
+	return v
+}
+
+func main() {
+	agent.Run(build, run, version)
+}
