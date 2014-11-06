b2db89d0e4146e5c1636e65e62b3260b38ac20c8
h2so5 <mail@h2so5.net>
2014-11-06 16:21:06 +0900

    Add c-clang cpp-clang

---
+++ images/cpp-clang/run.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+
+	"../../agent"
+)
+
+func build(files []string, in agent.Input, out *agent.Output) (string, []string) {
+	var args []string = []string{
+		"-o",
+		"main",
+		"-pthread",
+	}
+
+	if optim, ok := in.Options["optim"].(string); ok {
+		args = append(args, optim)
+	}
+
+	if optim, ok := in.Options["std"].(string); ok {
+		args = append(args, optim)
+	}
+
+	return "clang++", append(args, files...)
+}
+
+func run([]string, agent.Input, *agent.Output) (string, []string) {
+	return "./main", nil
+}
+
+var r = regexp.MustCompile("\\(.+\\)")
+
+func version() string {
+	_, v := agent.System(".", "", "clang++", "-v")
+	l := strings.Split(v, "\n")
+	v = l[0]
+        v = string(r.ReplaceAll([]byte(v), []byte("")))
+        v = strings.Replace(v, "Ubuntu", "", -1)
+	return v
+}
+
+func main() {
+	agent.Run(build, run, version)
+}
