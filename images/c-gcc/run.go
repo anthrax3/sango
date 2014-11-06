4c9c08f19ece4333239536279a064f5d649010a2
h2so5 <mail@h2so5.net>
2014-11-06 14:27:29 +0900

    Add c, objc

---
+++ images/c-gcc/run.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+
+	"../../agent"
+)
+
+func build(files []string, in agent.Input, out *agent.Output) (string, []string) {
+	var args []string = []string{
+		"-o",
+		"main",
+		"-pthread",
+	}
+
+	if optim, ok := in.Options["optim"].(string); ok {
+		args = append(args, optim)
+	}
+
+	if optim, ok := in.Options["std"].(string); ok {
+		args = append(args, optim)
+	}
+
+	return "gcc", append(args, files...)
+}
+
+func run([]string, agent.Input, *agent.Output) (string, []string) {
+	return "./main", nil
+}
+
+var r = regexp.MustCompile("\\(.+\\)")
+
+func version() string {
+	_, v := agent.System(".", "", "gcc", "-v")
+	l := strings.Split(v, "\n")
+	v = l[len(l)-2]
+	v = string(r.ReplaceAll([]byte(v), []byte("")))
+	return v
+}
+
+func main() {
+	agent.Run(build, run, version)
+}
---
+++ images/objc-gcc/run.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+
+	"../../agent"
+)
+
+func build(files []string, in agent.Input, out *agent.Output) (string, []string) {
+	var args []string = []string{
+		"-o",
+		"main",
+	}
+
+	args = append(args, "-fconstant-string-class=NSConstantString")
+
+        v, _ := agent.System(".", "", "gnustep-config", "--objc-flags")
+	v = strings.Replace(v, " -I/root/GNUstep/Library/Headers", "", -1)
+	v = strings.Replace(v, "\n", "", -1)
+	args = append(args, strings.Split(v, " ")...)
+
+	args = append(args, files...)
+	args = append(args, "-lgnustep-base")
+
+	v, _ = agent.System(".", "", "gnustep-config", "--objc-libs")
+	v = strings.Replace(v, "\n", "", -1)
+	args = append(args, strings.Split(v, " ")...)
+
+	return "gcc", args
+}
+
+func run([]string, agent.Input, *agent.Output) (string, []string) {
+	return "./main", nil
+}
+
+var r = regexp.MustCompile("\\(.+\\)")
+
+func version() string {
+	_, v := agent.System(".", "", "gcc", "-v")
+	l := strings.Split(v, "\n")
+	v = l[len(l)-2]
+	v = string(r.ReplaceAll([]byte(v), []byte("")))
+	return v
+}
+
+func main() {
+	agent.Run(build, run, version)
+}
