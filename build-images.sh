fa28a860be8bd51df01e5c633e3674c5dad9762c
h2so5 <mail@h2so5.net>
2014-11-08 15:31:39 +0900

    Add build script

---
+++ build-images.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for file in images/*/; do
+   if [ -d $file ]; then
+      if [ -f "$file/config.yml" ]; then
+          image=`cat $file/config.yml | grep id: | sed -r "s/id://g" | tr -d ' '`
+          echo "$image"
+          (cd $file && go get -d . ... && go build -o run && docker build -t "sango/$image" . && rm run);
+          docker push "sango/$image"
+          docker images -q --filter "dangling=true" | xargs docker rmi
+      fi
+   fi
+done
