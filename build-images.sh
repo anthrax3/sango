e578f5eb900577a8acca0f8b04315fe4d2d17936
h2so5 <mail@h2so5.net>
2014-11-13 14:55:51 +0900

    Do not use cache for builds

--- build-images.sh
+++ build-images.sh
@@ -5,7 +5,7 @@ for file in images/*/; do
       if [ -f "$file/config.yml" ]; then
           image=`cat $file/config.yml | grep id: | sed -r "s/id://g" | tr -d ' '`
           echo "$image"
-          (cd $file && go get -d . ... && go build -o run && docker build -t "sango/$image" . && rm run);
+          (cd $file && go get -d . ... && go build -o run && docker build --force-rm --no-cache -t "sango/$image" . && rm run);
           docker images -q --filter "dangling=true" | xargs docker rmi
       fi
    fi
