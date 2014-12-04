62e5bb9063407becab47439ab60b860e4d0297f9
h2so5 <mail@h2so5.net>
2014-12-04 23:54:20 +0900

    build-images.sh: Copy whole pkg into container

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+tgz=`mktemp -d`/sango.tar.gz
+tar czf $tgz --exclude-vcs .
+
 for file in images/[!_]*/; do
    if [ -d $file ]; then
       if [ -f "$file/config.yml" ]; then
           image=`cat $file/config.yml | grep id: | sed -r "s/id://g" | tr -d ' '`
           echo "$image"
-          (cd $file && go get -d . && go build -o agent && docker build -t sango/$image .);
+          (cd $file && cp $tgz . && docker build -t sango/$image . && rm sango.tar.gz);
           docker images -q --filter "dangling=true" | xargs docker rmi
       fi
    fi
